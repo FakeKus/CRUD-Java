a7bc1a3c6cceaba3aa38a88800efd180731b890e
FakeKus <88687338+FakeKus@users.noreply.github.com>
2025-03-09 04:56:31 -0300

    Initial

---
+++ src/main/java/com/example/forms/Student.java
@@ -0,0 +1,20 @@
+package com.example.forms;
+
+public class Student {
+
+    //Iniciando variáveis
+    private String name;
+    private char studentClass;
+
+    //Construtor vazio
+    public Student() {}
+    //Construtor
+    public Student(String name, char studentClass) {
+        this.name = name;
+        this.studentClass = studentClass;
+    }
+
+    
+
+
+}
